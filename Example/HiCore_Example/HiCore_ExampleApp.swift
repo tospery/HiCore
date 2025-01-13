c2fe266314dd12111a6219583bfcd457d51112c5
tospery <tospery@gmail.com>
2025-01-13 20:36:04 +0800

    1.0.9

---
+++ Example/HiCore_Example/HiCore_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiCore_ExampleApp.swift
+//  HiCore_Example
+//
+//  Created by 杨建祥 on 2025/1/13.
+//
+
+import SwiftUI
+
+@main
+struct HiCore_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- HiCore.podspec
+++ HiCore.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'HiCore'
-  s.version          = '1.0.8'
+  s.version          = '1.0.9'
   s.summary          = 'Core module.'
   s.description      = <<-DESC
 						Core module using Swift.
