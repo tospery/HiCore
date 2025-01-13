587125ea13dc6c85615dd47155968c1c6a190d13
tospery <tospery@gmail.com>
2025-01-13 23:06:49 +0800

    build

--- HiCore/Extension/Color+Core.swift
+++ HiCore/Extension/Color+Core.swift
@@ -28,15 +28,15 @@ public extension UIColor {
         return String(format: "#%02X%02X%02X%02X", r, g, b, a)
     }
     
-    var isDark: Bool {
-         var red: CGFloat = 0.0, green: CGFloat = 0.0, blue: CGFloat = 0.0
-         if getRed(&red, green: &green, blue: &blue, alpha: nil) {
-             let referenceValue: CGFloat = 0.411
-             let colorDelta = (red * 0.299) + (green * 0.587) + (blue * 0.114)
-             return 1.0 - colorDelta > referenceValue
-         }
-         return true
-     }
+//    var isDark: Bool {
+//         var red: CGFloat = 0.0, green: CGFloat = 0.0, blue: CGFloat = 0.0
+//         if getRed(&red, green: &green, blue: &blue, alpha: nil) {
+//             let referenceValue: CGFloat = 0.411
+//             let colorDelta = (red * 0.299) + (green * 0.587) + (blue * 0.114)
+//             return 1.0 - colorDelta > referenceValue
+//         }
+//         return true
+//     }
 
 }
 
