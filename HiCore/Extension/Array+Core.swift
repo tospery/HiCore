f756069aeba8d4f90fedafc5e4b8c0e5bda8f7be
tospery <tospery@gmail.com>
2026-04-22 13:33:08 +0800

    HiMapper

--- HiCore/Extension/Array+Core.swift
+++ HiCore/Extension/Array+Core.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import ObjectMapper
+import HiMapper
 
 // ******************************* MARK: - Array - Generics
 public extension Array {
--- HiCore/Extension/Dictionary+Core.swift
+++ HiCore/Extension/Dictionary+Core.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 import SwifterSwift
-import ObjectMapper
+import HiMapper
 import HiBase
 
 public extension Dictionary where Key == String {
